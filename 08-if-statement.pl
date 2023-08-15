b8329a5005c4abfafb8950ad080528841f7834ee
Agus-Coder <93748858+Agus-Coder@users.noreply.github.com>
2023-08-15 11:20:19 -0300

    [

--- 08-if-statement.pl
+++ 08-if-statement.pl
@@ -36,3 +36,47 @@ if($a == $b){
    print("a is less than b\n");
 }
 
+my %rates = (	
+     USD => 1,
+     YPY => 82.25,
+     EUR => 0.78,
+     GBP => 0.62,
+     CNY => 6.23
+);
+
+# print supported currencies
+print("Supported currency:\n");
+for(keys %rates){
+	print(uc($_),"\n");
+}
+
+# get inputs from users:
+print("Enter local currency:\n");
+$l_curr = <STDIN>; # <STDIN> it is used so the user can input data
+
+print("Enter foreign currency:\n");
+$f_curr = <STDIN>;
+
+print("Enter amount:\n");
+$l_amount = <STDIN>;
+
+chomp($l_curr,$f_curr,$l_amount); # Removes the final /n 
+
+
+# check user's inputs.
+if(not exists $rates{$l_curr}){
+     print("Local currency is not supported\n");
+}
+elsif(not exists $rates{$f_curr}){
+     print("Foreign currency is not supported\n");
+}
+else{
+     # convert from local currency to foreign currency
+     $f_amount = ($rates{$f_curr} / $rates{$l_curr}) * $l_amount;
+
+     # print out the result
+     print("$l_amount $l_curr = $f_amount $f_curr","\n");
+}
+
+
+# PERL UNLESS
